1590430147a7b2e47086f3c6269e4b34c895611f
Dyam Marcano <dyam.marcano@gmail.com>
2023-11-23 01:12:30 -0300

    Remove deprecated internal scraper package
    The scraper package was removed as it was deprecated and no longer needed in the project. This is part of overall refactoring and clean-up workflows. The go.sum file was also updated to reflect packages that are no longer required. All dependent calls and tests have been updated to match new changes.

--- internal/persistence/persistence_test.go
+++ internal/persistence/persistence_test.go
@@ -4,8 +4,8 @@ package persistence
 
 import (
 	"context"
-	v1 "documentDatabaseTest/internal/models/v1"
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"path/filepath"
 	"testing"
@@ -26,120 +26,27 @@ func init() {
 	}
 }
 
-func TestBadgerPersistence_GetStruct(t *testing.T) {
-	msgObj := &v1.OperationStatus{
-		RuntimeVersion: "1.0.0",
-		ID:             "831e1650-001e-001b-66ab-eeb76e069631",
-		OperationID:    "e8e564fd-38f5-4684-9581-c30f6c25213a",
-		Status:         "Failed",
-		CorrelationID:  "831e1650-001e-001b-66ab-eeb76e000000",
-		FileInfo: &v1.FileInfo{
-			ETag:          "0x8D4BCC2E4835CD0",
-			ContentType:   "application/octet-stream",
-			ContentLength: 524288,
-			Hash:          "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9",
-		},
-		Stages: &v1.Stages{
-			Stage1: &v1.Stage{
-				StartTime: "2017-06-26T18:41:00.9584103Z",
-				EndTime:   "2017-06-26T18:41:00.9584103Z",
-				Message:   "The request is invalid.",
-				InnerError: &v1.InnerError{
-					Date:    "2017-06-26T18:41:00",
-					Code:    "InvalidRequest",
-					Message: "File not meet the requirements.",
-				},
-			},
-			Stage2: &v1.Stage{
-				StartTime: "2017-06-26T18:41:00.9584103Z",
-				EndTime:   "2017-06-26T18:41:00.9584103Z",
-				Message:   "The request is invalid.",
-				InnerError: &v1.InnerError{
-					Date:    "2017-06-26T18:41:00",
-					Code:    "InvalidRequest",
-					Message: "File not meet the requirements.",
-				},
-			},
-			Stage3: &v1.Stage{
-				StartTime: "2017-06-26T18:41:00.9584103Z",
-				EndTime:   "2017-06-26T18:41:00.9584103Z",
-				Message:   "The request is invalid.",
-				InnerError: &v1.InnerError{
-					Date:    "2017-06-26T18:41:00",
-					Code:    "InvalidRequest",
-					Message: "File not meet the requirements.",
-				},
-			},
-		},
-	}
-
-	key, err := persistence.SetStruct(msgObj)
-	assert.NoErrorf(t, err, "expected error, but got '%s'", err)
-
-	got := &v1.OperationStatus{}
-	err = persistence.GetStruct(key, got)
-	assert.NoErrorf(t, err, "expected error, but got '%s'", err)
-
-	assert.Equalf(t, msgObj.FileInfo.ETag, got.FileInfo.ETag, "expected '%s', but got '%s'", msgObj.FileInfo.ETag, got.FileInfo.ETag)
-	fmt.Printf("key: %s, value: %v\n", key, got)
-}
-
 func TestBadgerPersistence_SetStructAsync(t *testing.T) {
-	msgObj := &v1.OperationStatus{
-		RuntimeVersion: "1.0.0",
-		ID:             "831e1650-001e-001b-66ab-eeb76e069631",
-		OperationID:    "e8e564fd-38f5-4684-9581-c30f6c25213a",
-		Status:         "Failed",
-		CorrelationID:  "831e1650-001e-001b-66ab-eeb76e000000",
-		FileInfo: &v1.FileInfo{
-			ETag:          "0x8D4BCC2E4835CD0",
-			ContentType:   "application/octet-stream",
-			ContentLength: 524288,
-			Hash:          "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9",
-		},
-		Stages: &v1.Stages{
-			Stage1: &v1.Stage{
-				StartTime: "2017-06-26T18:41:00.9584103Z",
-				EndTime:   "2017-06-26T18:41:00.9584103Z",
-				Message:   "The request is invalid.",
-				InnerError: &v1.InnerError{
-					Date:    "2017-06-26T18:41:00",
-					Code:    "InvalidRequest",
-					Message: "File not meet the requirements.",
-				},
-			},
-			Stage2: &v1.Stage{
-				StartTime: "2017-06-26T18:41:00.9584103Z",
-				EndTime:   "2017-06-26T18:41:00.9584103Z",
-				Message:   "The request is invalid.",
-				InnerError: &v1.InnerError{
-					Date:    "2017-06-26T18:41:00",
-					Code:    "InvalidRequest",
-					Message: "File not meet the requirements.",
-				},
-			},
-			Stage3: &v1.Stage{
-				StartTime: "2017-06-26T18:41:00.9584103Z",
-				EndTime:   "2017-06-26T18:41:00.9584103Z",
-				Message:   "The request is invalid.",
-				InnerError: &v1.InnerError{
-					Date:    "2017-06-26T18:41:00",
-					Code:    "InvalidRequest",
-					Message: "File not meet the requirements.",
-				},
-			},
-		},
+	msgObj := &struct {
+		Message string
+		Hash    string
+	}{
+		Message: "hello world",
+		Hash:    uuid.NewString(),
 	}
 
 	callbackFn := func(key string, err error) {
 		assert.NoErrorf(t, err, "expected error, but got '%s'", err)
 
-		got := &v1.OperationStatus{}
+		got := &struct {
+			Message string
+			Hash    string
+		}{}
 
 		err = persistence.GetStruct(key, got)
 		assert.NoErrorf(t, err, "expected error, but got '%s'", err)
 
-		assert.Equalf(t, msgObj.FileInfo.ETag, got.FileInfo.ETag, "expected '%s', but got '%s'", msgObj.FileInfo.ETag, got.FileInfo.ETag)
+		assert.Equalf(t, msgObj.Hash, got.Hash, "expected '%s', but got '%s'", msgObj.Hash, got.Hash)
 		fmt.Printf("key: %s, value: %v\n", key, got)
 	}
 
