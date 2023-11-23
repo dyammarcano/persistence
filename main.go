c8fc328b7d7b3682f3f9ccb7c5c82b6ebe939777
Dyam Marcano <dyam.marcano@gmail.com>
2023-11-23 01:21:30 -0300

    Simplify mockData function in main.go
    mockData function has been simplified to return a basic message with a UUID instead of complex operation status object. The "crypto/sha256" and "v1" models have been removed as they're no longer needed. The message object now directly serializes to JSON which is simpler to handle in downstream services.

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"context"
-	"crypto/sha256"
-	v1 "documentDatabaseTest/internal/models/v1"
 	"documentDatabaseTest/internal/persistence"
+	"encoding/json"
 	"github.com/caarlos0/log"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -14,55 +13,22 @@ import (
 )
 
 func mockData() ([]byte, error) {
-	msgObj := &v1.OperationStatus{
-		RuntimeVersion: "1.0.0",
-		ID:             uuid.NewString(),
-		OperationID:    uuid.NewString(),
-		Status:         "Failed",
-		CorrelationID:  uuid.NewString(),
-		FileInfo: &v1.FileInfo{
-			ETag:          "0x8D4BCC2E4835CD0",
-			ContentType:   "application/octet-stream",
-			ContentLength: 524288,
-			Hash:          string(sha256.New().Sum(uuid.NodeID())),
-		},
-		Stages: &v1.Stages{
-			Stage1: &v1.Stage{
-				StartTime: "2017-06-26T18:41:00.9584103Z",
-				EndTime:   "2017-06-26T18:41:00.9584103Z",
-				Message:   "The request is invalid.",
-				InnerError: &v1.InnerError{
-					Date:    "2017-06-26T18:41:00",
-					Code:    "InvalidRequest",
-					Message: "File not meet the requirements.",
-				},
-			},
-			Stage2: &v1.Stage{
-				StartTime: "2017-06-26T18:41:00.9584103Z",
-				EndTime:   "2017-06-26T18:41:00.9584103Z",
-				Message:   "The request is invalid.",
-				InnerError: &v1.InnerError{
-					Date:    "2017-06-26T18:41:00",
-					Code:    "InvalidRequest",
-					Message: "File not meet the requirements.",
-				},
-			},
-			Stage3: &v1.Stage{
-				StartTime: "2017-06-26T18:41:00.9584103Z",
-				EndTime:   "2017-06-26T18:41:00.9584103Z",
-				Message:   "The request is invalid.",
-				InnerError: &v1.InnerError{
-					Date:    "2017-06-26T18:41:00",
-					Code:    "InvalidRequest",
-					Message: "File not meet the requirements.",
-				},
-			},
-		},
+	msgObj := &struct {
+		Message string
+		Hash    string
+	}{
+		Message: "hello world",
+		Hash:    uuid.NewString(),
 	}
 
-	log.Infof("Mock data: %s", msgObj.OperationID)
+	log.Infof("Mock data: %s", msgObj.Hash)
 
-	return msgObj.SerializeBytes()
+	data, err := json.Marshal(msgObj)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
 
 func main() {
